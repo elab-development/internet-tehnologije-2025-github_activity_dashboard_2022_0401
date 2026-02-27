651caaad40a32604cbb8a71ae24c2d56cbb6811b
Bajolaprogramer <bajic.fon@gmail.com>
2026-02-27 03:00:44 +0100

    Dodate security protekcije

--- packages/index.ts
+++ packages/index.ts
@@ -2,23 +2,40 @@ import cors from "cors";
 import "reflect-metadata";
 import express from "express";
 import dotenv from "dotenv";
-
-dotenv.config({ path: "packages/.env" });
-dotenv.config();
-
 import { dataSource } from "./database/data-src";
 import authRoutes from "./modules/auth/auth.routes";
 import repositoriesRoutes from "./modules/repositories/repositories.routes";
 import githubRoutes from "./modules/github/github.routes";
 
-const port = process.env.PORT || 5000;
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 export const app = express(); // 👈 BITNO
+
+
+app.use(helmet());
+app.use(express.json());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+
+
+
+dotenv.config({ path: "packages/.env" });
+dotenv.config();
+
+
+const port = process.env.PORT || 5000;
+
 
 app.use(
   cors({
     origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
